961b73bcb77c33605d98c7253d9e78155397ccfd
unknown <cleber.almeida@hotmail.com>
2022-02-07 22:56:31 -0400

    Subir o Menu está faltando estender completo

--- src/componentes/navegacao/Elementos.js
+++ src/componentes/navegacao/Elementos.js
@@ -6,7 +6,7 @@ function Elementos(props) {
             <Link to={props.link} className={Style.Link}>
                 <div className={Style.Area}>
                     <div className={Style.Texto}>{props.texto}</div>
-                    <div className={Style.Imagem}><img src={props.caminho} alt={'img' + props.texto} /></div>
+                    <div className={Style.Imagem}>{props.imagem}</div>
                 </div>
             </Link>
         </div>
--- src/componentes/navegacao/NaviBar.js
+++ src/componentes/navegacao/NaviBar.js
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import Elementos from './Elementos'
 import Style from './NaviBar.module.css'
-import ImgCarteira from '../img/wallet24.png'
+import { FaWallet } from 'react-icons/fa'
+import { AiOutlineAreaChart, AiOutlineExclamationCircle, AiOutlineLogout, AiOutlineUser, AiOutlineMenu } from 'react-icons/ai'
+
 function NaviBar() {
+    const [local, setLocal] = useState(Style.Esconder)
     return (
-        <div className={Style.Geral}>
-            <Elementos link="/" texto="Login" caminho={ImgCarteira} />
-            <Elementos link="/corretoras" texto="Corretoras" caminho={ImgCarteira} />
-            <Elementos link="/sobre" texto="Sobre" caminho={ImgCarteira} />
-            <Elementos link="/usuario" texto="Usuário" caminho={ImgCarteira} />
-            <Elementos link="/sair" texto="Sair" caminho={ImgCarteira} />
+        <div className={Style.Geral, local}>
+            <div className={Style.Titulo}><div>Sis. Acom. Investimento</div><div className={Style.BtMenu} onClick={DeslocarMenu}>{<AiOutlineMenu />}</div></div>
+            <Elementos link="/" texto="Carteira" imagem={<FaWallet />} />
+            <Elementos link="/corretoras" texto="Corretoras" imagem={<AiOutlineAreaChart />} />
+            <Elementos link="/sobre" texto="Sobre" imagem={<AiOutlineExclamationCircle />} />
+            <Elementos link="/usuario" texto="Usuário" imagem={<AiOutlineUser />} />
+            <Elementos link="/sair" texto="Sair" imagem={<AiOutlineLogout />} />
         </div>
     )
+    function DeslocarMenu() {
+        local == Style.Esconder ?
+            setLocal(Style.Mostrar)
+            :
+            setLocal(Style.Esconder)
+    }
 }
+
 export default NaviBar
